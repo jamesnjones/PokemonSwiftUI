51e9d7be0b75acdd276356f328f71c35565343f7
James <james>
2021-08-30 23:46:20 +0100

    Initial Commit

---
+++ PokemonSwiftUI/PokemonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSwiftUIApp.swift
+//  PokemonSwiftUI
+//
+//  Created by james Jones on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
