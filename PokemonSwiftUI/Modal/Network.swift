1d9bf48623706af14cbc388bf32ddfd6982a5588
James <james>
2022-07-12 20:34:59 +0100

    commit

---
+++ PokemonSwiftUI/Modal/Network.swift
@@ -0,0 +1,50 @@
+//
+//  Network.swift
+//  PokemonSwiftUI
+//
+//  Created by james Jones on 12/07/2022.
+//
+
+import Foundation
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct User: Codable, Hashable {
+    var sprites: Sprites?
+    var name: String?
+    var abilities: [Abilities]?
+    var stats: [Stats]?
+    var moves: [Moves]?
+}
+
+struct Sprites: Codable, Hashable {
+    // swiftlint:disable:next identifier_name
+    var front_default: String?
+}
+
+struct Result: Codable, Hashable {
+    var name: String
+}
+
+struct Abilities: Codable, Hashable {
+    var ability: Ability
+}
+
+struct Ability: Codable, Hashable {
+    var name: String
+}
+
+struct Stats: Codable, Hashable {
+    // swiftlint:disable:next identifier_name
+    var base_stat: Int
+}
+
+struct Moves: Codable, Hashable {
+    var move: Move
+}
+
+struct Move: Codable, Hashable {
+    var name: String
+}
